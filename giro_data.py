c478e5d65b6b12b40e9f395d59b2b80871632b99
Gerard Piccini <gerard.piccini@scranton.edu>
2026-02-25 12:08:24 -0500

    giro data module

---
+++ giro_data.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import os
+import datetime as dt
+import numpy as np
+
+defaultFromDate = (dt.datetime.now(dt.timezone.utc).date() - dt.timedelta(days=int(7))).strftime('%Y-%m-%dT%H:%M:%S')
+defaulttoDate = (dt.datetime.now(dt.timezone.utc).date() + dt.timedelta(days=int(1))).strftime('%Y-%m-%dT%H:%M:%S')
+
+def get_giro_data(fromDate=defaultFromDate,toDate=defaulttoDate,stationList=['AL945','MHJ45']):
+    urlfrom = '&fromDate=' + fromDate
+    urlto = '&toDate=' + toDate
+    urldates = urlfrom + urlto
+
+    urlpt1 = "https://lgdc.uml.edu/common/DIDBGetValues?ursiCode="
+    urlpt2 = "&charName=foF2,hmF2,foE,MUFD,hF2&DMUF=3000"#"&charName=MUFD,hmF2,TEC,foF2,foE,foEs&DMUF=3000"
+
+    df_list = []
+    for station in stationList:
+        df=pd.read_csv(urlpt1 + station + urlpt2 + urldates,
+                comment='#',
+                delim_whitespace=True,
+                parse_dates=[0],
+                names = ['time', 'cs', 'fof2', 'qd1', 'hmF2', 'qd2', 'foE', 'qd3', 'MUFD', 'qd4', 'hF2', 'qd5'])\
+            .assign(station_id=station)
+        df_list.append(df)
+
+    giro_data=pd.concat(df_list)
+
+    giro_data = giro_data[['station_id', 'time', 'cs', 'fof2', 'hmF2', 'foE', 'MUFD', 'hF2']]
+
+    giro_data.cs = giro_data.cs.astype(str)
+    giro_data = giro_data[giro_data.cs.str.contains("No") == False]
+    giro_data['time'] = pd.to_datetime(giro_data['time'],format='ISO8601')
+
+    #set --- to nan
+    giro_data = giro_data.applymap(lambda x: np.nan if type(x) is str and x == '---' else x)
+
+    giro_data.sort_values(by=['time'], inplace=True)
+
+    return giro_data
+
+def update_giro_data_csv(giro_data_path,stationList=['AL945','MHJ45']):
+    if os.path.isfile(giro_data_path):
+        old_data = pd.read_csv(giro_data_path)
+        old_data['time'] = pd.to_datetime(old_data['time'],format='ISO8601')
+        fromDate = (old_data['time'].iloc[-1]+ dt.timedelta(minutes=1)).strftime('%Y-%m-%dT%H:%M:%S')
+    else:
+        fromDate = (dt.datetime.now(dt.timezone.utc).date() - dt.timedelta(days=int(7))).strftime('%Y-%m-%dT%H:%M:%S')
+    toDate = (dt.datetime.now(dt.timezone.utc).date() + dt.timedelta(days=int(1))).strftime('%Y-%m-%dT%H:%M:%S')
+
+    giro_data = get_giro_data(fromDate,toDate,stationList)
+
+    if os.path.isfile(giro_data_path):
+        giro_data = pd.concat([old_data,giro_data])
+
+    giro_data.to_csv(giro_data_path,index=False)
+
+if __name__ == '__main__':
+    print(defaultFromDate)
+    testtime = dt.datetime.now(dt.timezone.utc).strftime('%Y-%m-%dT%H:%M:%S')
+    print(type(testtime))
+
+    update_giro_data_csv(giro_data_path='Station Data.csv',stationList=['AL945','MHJ45'])
